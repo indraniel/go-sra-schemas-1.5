76d4b26caf1302714e449b10553967e634728bd1
Niel Das <idas@Niels-MBP.attlocal.net>
2015-02-22 19:09:32 -0600

    + update the perl inline file changes
      - add an additional newline after the "struct" insert statement
      - use Perl's -n option instead of the -p for the line insertion
        command

--- get-sra-schemas.sh
+++ get-sra-schemas.sh
@@ -31,5 +31,5 @@ xsd-makepkg -basepath=github.com/indraniel/go-sra-schemas-1.5 -gofmt=true -local
 # Not sure if this is a bug or not...
 
 for gofile in $(find . -name "*.go" -print); do
-    perl -p -i -e 'if ($. == 11) { print "type XsdtString struct{ string }" } else { print $_ }' $gofile
+    perl -n -i -e 'if ($. == 11) { print "type XsdtString struct{ string }\n\n" } else { print $_ }' $gofile
 done
